48db59679bcf43a558e809b77c6bafb23f907c61
Esteban Ginez <175813+eginez@users.noreply.github.com>
2024-07-13 21:34:44 -0700

    fixing a few things

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,49 @@
-use std::cmp::max;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-// I need a struct to hold a compact representation of a bitstring
-// and operations on it
-
 struct Bitstring {
     // Packs the strings into bits to
     // and operates on the bits directly
     data: Vec<u8>,
+    length: usize,
 }
 
 impl Bitstring {
     pub fn from_string(data: &str) -> Self {
         let len = data.chars().count();
-        let capacity = max(1, len / 8);
-        println!("creating an array of size {:?}", capacity);
+        let capacity = (len + 7) / 8;
+        println!(
+            "creating an array of size {:?}, len: {:?}",
+            capacity,
+            len / 8
+        );
 
-        let mut current_num: u8 = 0;
-        let mut data_vec = vec![0; capacity];
+        let mut data_vec = vec![0u8; capacity];
         for (pos, char) in data.char_indices() {
-            let value = match char {
+            let byte_index = pos / 8;
+            let bit_index = pos % 8;
+            let bit = match char {
                 '0' => 0,
                 '1' => 1,
                 _ => panic!("Unexpected character '{}' at position {}", char, pos),
             };
-            current_num = current_num | (value << pos);
+            data_vec[byte_index] |= bit << bit_index;
         }
-        data_vec.push(current_num);
-        Bitstring { data: data_vec }
+
+        Bitstring {
+            data: data_vec,
+            length: len,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn create() {
-        let bs = Bitstring::from_string("0110");
+    #[rstest]
+    fn create(#[values("1010", "11001", "000111", "111111111")] input: &str) {
+        let bs = Bitstring::from_string(input);
         println!("{:?}", bs.data);
-        assert!(!bs.data.is_empty());
+        assert!(bs.data.len() >= 1 && bs.data.len() <= 2);
+        assert!(bs.length == input.chars().count());
     }
 }
